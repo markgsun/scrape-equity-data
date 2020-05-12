d55f5623ad182e44f1f7c09924ffba6eedcd03f0
Mark <mark.g.sun@gmail.com>
2020-05-12 07:29:35 -0400

    Updated yahoo_scrape with main gate tests

--- yahoo_scrape.py
+++ yahoo_scrape.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 Created on Fri May  8 07:07:03 2020
 
 @author: markg
 
 Pulls historical price table from Yahoo finance for particular stock and date range
-"""
+'''
 
 # Packages
 import datetime as dt
 import lxml
 import pandas as pd
 import requests
+import scrape
 import time
 from lxml import html
 
@@ -25,29 +26,9 @@ def dt2epoch(dt_datetime):
 
 # Subdomain
 def subdomain(symbol, start, end, filter='history'):
-     subdoma="/quote/{0}/history?period1={1}&period2={2}&interval=1d&filter={3}&frequency=1d"
+     subdoma='/quote/{0}/history?period1={1}&period2={2}&interval=1d&filter={3}&frequency=1d'
      subdomain = subdoma.format(symbol, start, end, filter)
      return subdomain
-
-# Header
-def header_function(subdomain):
-    hdrs =  {"authority": "finance.yahoo.com",
-             "method": "GET",
-             "path": subdomain,
-             "scheme": "https",
-             "accept": "text/html",
-             "accept-encoding": "gzip, deflate, br",
-             "accept-language": "en-US,en;q=0.9",
-             "cache-control": "no-cache",
-             "cookie": "Cookie:identifier",
-             "dnt": "1",
-             "pragma": "no-cache",
-             "sec-fetch-mode": "navigate",
-             "sec-fetch-site": "same-origin",
-             "sec-fetch-user": "?1",
-             "upgrade-insecure-requests": "1",
-             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"}
-    return hdrs
 
 # Main function
 def yahoo_hist_px(stock, day_end, day_start):
@@ -55,6 +36,9 @@ def yahoo_hist_px(stock, day_end, day_start):
     dt_temp = dt.datetime.strptime(day_end,'%Y%m%d')
     dt_start = dt.datetime.strptime(day_start,'%Y%m%d')
     start = dt2epoch(dt_start)
+    
+    # Clean stock ticker
+    stock = stock.replace('.','-')
     
     # Initialize price table
     px_table = None
@@ -65,12 +49,12 @@ def yahoo_hist_px(stock, day_end, day_start):
         end = dt2epoch(dt_end)
         
         # Domain and header
+        base_url = 'https://finance.yahoo.com'
         sub = subdomain(stock, start, end)
-        header = header_function(sub)
+        url = base_url + sub
+        header = scrape.header_function(base_url, sub)
         
         # Send request
-        base_url = 'https://finance.yahoo.com'
-        url = base_url + sub
         page = requests.get(url, headers=header)
         
         # Read webpage
@@ -108,3 +92,8 @@ def yahoo_hist_px(stock, day_end, day_start):
     px_table.columns = px_table.columns.str.replace('*','')
     
     return px_table
+
+# Execution
+if __name__ == '__main__':
+    # Test
+    yahoo_hist_px('KSS','20200501','20200101')
